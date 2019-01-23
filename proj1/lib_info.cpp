ea8172d0b3a881c7b5a668bc7b04150c320761f2
Jazmin Alexis Gurganus <jgurganu@tesla11.eecs.utk.edu>
2019-01-22 19:34:00 -0500

    more changes

--- proj1/lib_info.cpp
+++ proj1/lib_info.cpp
@@ -1,41 +1,51 @@
 #include <iostream>
 #include "lib_info.h"
 #include <fstream>
-<<<<<<< HEAD
 #include <sstream>
-=======
 #include <algorithm>
->>>>>>> 68db9f591d4b6be505438fd147646beeb67d1342
 
 using namespace std;
+
+int timeToInt(string time);
+string convertUnderscores(string line);
+
 
 int main(int argc, char *argv[]){
 //Read in lines. Convert each time to an integer and test.
 	
 	ifstream fin;
 	fin.open(argv[1]);
-<<<<<<< HEAD
 	string line, name, title,  artist, album, genre, track;
 	istringstream buffer;
+	string stringtime;
 	int time;
-//title>>time>>artist>>album>>genre>>track;
-	
 	while (getline(fin, line)){
 		buffer.clear();
 		buffer.str(line);
-		buffer >> title >> time >> artist >> album >> genre >> track;
-		cout << title << time << artist << album << genre << track;
-	
+		buffer >> title >> stringtime >> artist >> album >> genre >> track;		
+		title = convertUnderscores(title);
+		cout << title << endl;
+		time = timeToInt(stringtime);
+		
 	}
 	cout <<"end";
-=======
-//	fin>>		
+
+
+	return 0;
+}
+
+int timeToInt(string time){
+	int hour, min;
+	char discard;
+	istringstream buffer;
+	buffer.str(time);
+	buffer >> hour >> discard >> min;
+	int intTime = (hour * 60) + min;
+	return intTime;
+}
 
 string convertUnderscores(string line){
 	replace(line.begin(), line.end(), '_',' ');
 	return line;
 }
 
->>>>>>> 68db9f591d4b6be505438fd147646beeb67d1342
-	return 0;
-}
--- proj1/lib_info.h
+++ proj1/lib_info.h
@@ -2,32 +2,15 @@
 #include <map>
 using namespace std;
 
-<<<<<<< HEAD
-class Song { 
-  public:
-    string title;
-    int time;
-    int track;
-};
-
-class Album {
-  public:
-    map <int, Song *> songs;
-    string name;
-    int time;
-};
-
-class Artist {
-  public:
-    map <string, Album *> albums;
-    string name;
-=======
 struct Song { 
+public:
     string title;
     int time;
     int track;
-		void printSong();
-		string convertUnderscores(string line);
+	void printSong();
+
+
+
 };
 
 struct Album {
@@ -43,7 +26,7 @@ struct Artist {
 		string checkArtist(); //checks to see if Artist is in map
 		string createArtist(); //creates an artist if nonexistent
 		string name;
->>>>>>> 68db9f591d4b6be505438fd147646beeb67d1342
     int time;
     int nsongs;
 };
+
